03d970ff07e00139668cf95cce072aadc36f8098
flocka12 <muriithiijose@gmail.com>
2019-03-16 12:49:50 +0300

    [Finishes#164629239]Test passes and intergrates travis

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from instance.config import APP_CONFIG
+from DB.queries import create_tables
 from .api.v1 import VERSION_ONE
 
 def create_app(config_name):
     ''' creates app and registers blueprints '''
     app = Flask(__name__)
+    create_tables()
     JWTManager(app)
     app.register_blueprint(VERSION_ONE)
     app.config.from_object(APP_CONFIG[config_name])
+
     return app
